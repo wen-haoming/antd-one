a22bf2f0c76a0bf7db6761cfee4d496fe4395cf3
haoming <435203093@qq.com>
2022-06-27 10:13:45 +0800

    Update index.tsx

--- src/Components/TableFormRender/index.tsx
+++ src/Components/TableFormRender/index.tsx
@@ -12,49 +12,6 @@ import type { Field, FieldFunc } from '../FormRender/types';
 import { getSplitCol } from './utils';
 import { SheetComponent } from '@antv/s2-react';
 
-// 1. 准备数据
-const data = [
-  {
-    province: '浙江',
-    city: '杭州',
-    type: '家具',
-    sub_type: '桌子',
-    price: '1',
-  },
-  {
-    province: '浙江',
-    city: '杭州',
-    type: '家具',
-    sub_type: '沙发',
-    price: '2',
-  },
-  {
-    province: '浙江',
-    city: '杭州',
-    type: '办公用品',
-    sub_type: '笔',
-    price: '3',
-  },
-  {
-    province: '浙江',
-    city: '杭州',
-    type: '办公用品',
-    sub_type: '纸张',
-    price: '4',
-  },
-];
-
-// 2. 配置数据
-const dataCfg = {
-  fields: {
-    // rows: ['province', 'city','type','sub_type','price'],
-    columns: ['type', 'city', 'sub_type', 'province', 'price'],
-    // values: ['price'],
-  },
-  data,
-};
-
-
 export interface TableFormRenderProps {
   request: (
     pageData: { current: number; pageSize: number },
@@ -128,10 +85,7 @@ const TableFormRender: FC<TableFormRenderProps> = (props) => {
           colProps={{ span: responsiveCol }}
         />
       </Form>
-      {/* <XTable columns={columns} {...tableRequest.tableProps} {...restTableProps}  /> */}
-      <SheetComponent loading={tableRequest.loading} onClick={(item) => {
-        console.log('item', item);
-      }} sheetType="table" dataCfg={dataCfg} adaptive />
+      <XTable columns={columns} {...tableRequest.tableProps} {...restTableProps}  />
     </>
   );
 };
